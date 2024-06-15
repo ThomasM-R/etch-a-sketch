57478fd750bc150433b89443b9d248ba9f33fcca
Thomas <139985524+ThomasM-R@users.noreply.github.com>
2024-06-15 18:21:14 -0400

    final touches and features

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const main = document.querySelector("main");
 
 // generate grid
 function createGrid(size) {
-	main.innerHTML = "";
+	main.innerHTML = ""; // hacky way to delete everything inside of <main>
 	for (let y = 0; y < size; y++) {
 		for (let x = 0; x < size; x++) {
 			let div = document.createElement("div");
 			div.style.backgroundColor = ["lightgray", "white"][(x + y) % 2];
+
+			div.addEventListener("mouseenter", ()=>{
+				div.animate([
+					{filter: "brightness(0)"},
+					{filter: "brightness(1)"},
+				], {
+					duration: 10000,
+					iterations: 1,
+				});
+			});
+
 			main.appendChild(div);
 		}
 	}
+
+	main.style.setProperty('--size', size);
 }
 
 createGrid(16);
